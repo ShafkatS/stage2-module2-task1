92de208388cd31b15e7cb6fb9c56dbfba2ee43b6
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-06-05 22:06:15 +0500

    Update AddUserServlet.java

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -13,13 +13,13 @@ import java.io.IOException;
 
 @WebServlet("/add")
 public class AddUserServlet extends HttpServlet {
-     @Override
+     
+     
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         req.getRequestDispatcher("/jsp/add.jsp").forward(req, resp);
     }
 
-    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         String firstName = req.getParameter("firstName");
